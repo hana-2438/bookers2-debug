baf19b379d630ed0d255c8ac69c5663ef377680d
EC2 Default User <ec2-user@ip-172-31-11-216.ap-northeast-1.compute.internal>
2023-01-27 11:53:24 +0000

    [Start]bookers2-Task3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to = 'homes#top'
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+    
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
