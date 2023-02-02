8062ae54654159eaa35bc2e19eee5fd27b68f291
EC2 Default User <ec2-user@ip-172-31-11-216.ap-northeast-1.compute.internal>
2023-02-02 02:31:00 +0000

    [Start]bookers2-Task6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,14 @@ import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
 
+
+// 以下を追加する
+//= require jquery
+require("@rails/ujs").start()
+// require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
