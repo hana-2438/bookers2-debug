91ac3f8152818e1b654e69d5669500cfc0c91f8f
EC2 Default User <ec2-user@ip-172-31-11-216.ap-northeast-1.compute.internal>
2023-02-04 08:10:20 +0000

    [Update]bookers2-Task8a

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,26 +1,26 @@
 class ChatsController < ApplicationController
-  before_action :reject_non_related, only: [:show]
+  before_action :reject_non_related, only: [:show]#コントローラーの各アクションを行う前に、相互フォローか判断する
   
   def show
-    @user = User.find(params[:id])
-    rooms = current_user.user_rooms.pluck(:room_id)
-    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    @user = User.find(params[:id])#チャットする相手が誰なのか
+    rooms = current_user.user_rooms.pluck(:room_id)#ログイン中のユーザー（自分）の部屋情報をすべて取得し、roomsに代入
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)#find_byメソッドを使ってチャットする相手とのルームがあるか確認し、それをrser_roomsに代入する
     
-    unless user_rooms.nil?
-      @room = user_rooms.room
-    else
-      @room = Room.new
-      @room.save
-      UserRoom.create(user_id: current_user.id, room_id: @room.id)
-      UserRoom.create(user_id: @user.id, room_id: @room.id)
+    unless user_rooms.nil?#ユーザールームはありましたか？
+      @room = user_rooms.room#ルームがあった場合、@roomにユーザー（自分と相手）と紐づいているroomを代入する
+    else#ユーザールームがなかった場合
+      @room = Room.new#新しくルームを作成
+      @room.save#保存する
+      UserRoom.create(user_id: current_user.id, room_id: @room.id)#自分のUserRoomモデル作成
+      UserRoom.create(user_id: @user.id, room_id: @room.id)#相手のUserRoomモデル作成
     end
-    @chats = @room.chats
-    @chat = Chat.new(room_id: @room.id)
+    @chats = @room.chats#チャットの一覧用変数
+    @chat = Chat.new(room_id: @room.id)#チャットの投稿用の変数
   end
   
   def create
-    @chat = current_user.chats.new(chat_params)
-    render :validater unless @chat.save
+    @chat = current_user.chats.new(chat_params)#
+    render :validater unless @chat.save#もし、新規投稿が保存されなかった場合、validater.js.erbを探す。（非同期の使用）
   end
   
   
